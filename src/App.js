55808026f5f6a7ec92ca8944670952e4177fce22
shreyapandey777 <pandeyshreya3851@gmail.com>
2024-07-03 01:30:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Todowrapper } from './components/todowrapper';
+
+function App() {
+  return (
+    <div className="App">
+    <Todowrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/todowrapper.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { TodoForm } from './todoForm'
+export  const Todowrapper = () => {
+    return  (
+    <div className ='TodoWrapper'>
+        <TodoForm/> </div> 
+    )
+}
